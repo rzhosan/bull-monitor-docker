ceedc91cbea14a3ec50426cf9f3770562c48c204
Rostislav Zhosan <rostislav.zhosan@gmail.com>
2022-12-09 09:54:39 +0100

    Handle Sigterm

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,18 @@ if [[ -z "${QUEUES}" ]]; then
   exit 1
 fi
 
+pid=0
+
+term_handler() {
+  if [ $pid -ne 0 ]; then
+    kill -SIGTERM "$pid"
+    wait "$pid"
+  fi
+  exit 0;
+}
+
+trap 'kill ${!}; term_handler' SIGINT SIGTERM EXIT
+
 
 bull-monitor \
   --redis-uri "redis://${REDIS_HOST}:${REDIS_PORT}" \
@@ -15,4 +27,11 @@ bull-monitor \
   --max-metrics=${MAX_METRICS} \
   --metrics-interval=${METRICS_INTERVAL} \
   --host="0.0.0.0" \
-  --queue ${QUEUES}
+  --queue ${QUEUES} &
+
+pid="$!"
+
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
